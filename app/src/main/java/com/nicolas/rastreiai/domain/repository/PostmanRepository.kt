e2c2237bf5dd43fef4deb8222938bb803cfc1c1d
Aleixo-Dev <nicolasaleixo2020@gmail.com>
2021-11-01 15:41:25 -0300

    🗃 repository

--- app/src/main/java/com/nicolas/rastreiai/domain/repository/PostmanRepository.kt
+++ app/src/main/java/com/nicolas/rastreiai/domain/repository/PostmanRepository.kt
@@ -1,10 +1,20 @@
 package com.nicolas.rastreiai.domain.repository
 
+import com.nicolas.rastreiai.domain.model.OrderEntity
 import com.nicolas.rastreiai.domain.model.OrderRequest
 import com.nicolas.rastreiai.domain.model.OrderStateUiDomain
+import kotlinx.coroutines.flow.Flow
 
 interface PostmanRepository {
 
     suspend fun getOrderState(orderRequest: OrderRequest) : List<OrderStateUiDomain>
+
+    fun getOrders() : Flow<List<OrderEntity>>
+
+    suspend fun getOrderById(id: Int): OrderEntity?
+
+    suspend fun insertOrder(note: OrderEntity)
+
+    suspend fun deleteOrder(note: OrderEntity)
 
 }
